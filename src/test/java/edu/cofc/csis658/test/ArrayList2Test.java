2ac4a55bee2e2ff977fc08fa8cc942dff143bae7
Keith Briggs <kabriggs991@gmail.com>
2016-03-31 19:12:14 -0400

    updated test

--- src/test/java/edu/cofc/csis658/test/ArrayList2Test.java
+++ src/test/java/edu/cofc/csis658/test/ArrayList2Test.java
@@ -1,5 +1,38 @@
 package edu.cofc.csis658.test;
 
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ArrayList2Test {
+	
+	private Set<String> setA;
+	
+	@Before
+	public void setup() {
+		int indexNumber = 4;
+		setA = new HashSet<String>();
+		
+	}
+	
+	@Test
+	public void addAllNumMovedGreaterZero() {
+		System.out.println("test begin");
+		ArrayList2<String> ar2 = new ArrayList2();
+		
+		setA.add("String1");
+		setA.add("String2");
+		setA.add("String3");
+		
+		boolean mod = ar2.addAll(1, setA);
+	
+		assertTrue(mod);
+	
+		System.out.println("test end");
+	}
 
 }
