05ce00bc9d2919f619a0c188c990bb513053d7bb
husnasidd <siddiquih@g.cofc.edu>
2016-04-04 12:44:12 -0400

    tests for indexOf

---
+++ src/test/java/edu/cofc/csis658/test/ArrayList2_indexOf_Test.java
@@ -0,0 +1,74 @@
+package edu.cofc.csis658.test;
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayList2_indexOf_Test {
+
+		private Set<String> setA;
+		
+		@Before
+		public void setup() {
+			
+			
+		}
+		
+		@Test
+		public void indexOfFirstOccurrenceOfElement() {
+			System.out.println("test begin");
+			ArrayList2<Integer> testArray = new ArrayList2();
+			
+			testArray.add(1);
+			testArray.add(4);
+			testArray.add(null);
+			testArray.add(4);
+			testArray.add(6);
+		
+			int returnValue = testArray.indexOf(4);
+		
+			assertEquals(1,returnValue);
+		
+			System.out.println("test end");
+		}
+		
+		@Test
+		public void indexOfElementNotInList() {
+			System.out.println("test begin");
+			ArrayList2<Integer> testArray = new ArrayList2();
+			
+			testArray.add(1);
+			testArray.add(4);
+			testArray.add(null);
+			testArray.add(6);
+		
+			int returnValue = testArray.indexOf(3);
+		
+			assertEquals(-1,returnValue);
+		
+			System.out.println("test end");
+		}
+		
+		@Test
+		public void indexOfNull() {
+			System.out.println("test begin");
+			ArrayList2<Integer> testArray = new ArrayList2();
+			
+			testArray.add(1);
+			testArray.add(4);
+			testArray.add(null);
+			testArray.add(6);	
+		
+			int returnValue = testArray.indexOf(null);
+		
+			assertEquals(2,returnValue);
+		
+			System.out.println("test end");
+		}
+		
+
+	
+}
