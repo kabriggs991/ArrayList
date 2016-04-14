48db1373cfec1dd0f6c1f965520a05b30fdb9474
Charles Powell <lilbrudder127@MacBook-Pro.local>
2016-04-14 00:01:19 -0400

    Worked on remove test

--- src/test/java/edu/cofc/csis658/test/ArrayList2_remove_Test.java
+++ src/test/java/edu/cofc/csis658/test/ArrayList2_remove_Test.java
@@ -12,35 +12,51 @@ import org.junit.Test;
 public class ArrayList2_remove_Test {
 	
 	@Test
-	public void addAllNumMovedGreaterZero() {
-		System.out.println("test begin");
-		ArrayList2<String> ar2 = new ArrayList2();
+	public void numMovedEqualToZero() {
+		System.out.println("test: numMovedEqualToZero start");
+		ArrayList2<String> ar = new ArrayList2();
 		
-		ar2.add("string");
+		ar.add("element");
 
-		ar2.remove(0);
-
-		assertTrue(ar2.size() == 0);
+		assert(ar.size() == 1);
+		
+		ar.remove(0);
+		assert(ar.size() == 0);
 	
-		System.out.println("test end");
+		System.out.println("test: numMovedEqualToZero end ");
 	}
 
 	
 	@Test
-	public void addAllNumMovedGreaterZeo() {
-		System.out.println("test begin");
+	public void numMovedGreaterThanZero() {
+		System.out.println("test: numMovedGreaterThanZero start");
 		ArrayList2<String> ar2 = new ArrayList2();
 		
 		ar2.add("string");
 		ar2.add("string");
 		ar2.add("string");
-		ar2.remove(0);
+		
+		assert(ar2.size() == 3);
+		
+		ar2.remove(1);
 
-		assertTrue(ar2.size() == 0);
+		assert(ar2.size() == 2);
 	
-		System.out.println("test end");
+		System.out.println("test: numMovedGreaterThanZero end");
 	}
+		
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void numMovedLessThanZero() {
+		System.out.println("test: numMovedLessThanZero start");
+		ArrayList2<String> ar3 = new ArrayList2();
+		
 
+		ar3.add("string");
 		
-		
+		ar3.remove(1);
+
+	
+		System.out.println("test: numMovedLessThanZero end");
+	}
+	
 }
