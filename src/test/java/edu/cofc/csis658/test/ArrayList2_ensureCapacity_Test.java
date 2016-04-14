be8836c6cdb3f983a5a4808daeb022f696149c66
Keith Briggs <kabriggs991@gmail.com>
2016-04-13 22:48:10 -0400

    working test to check for capacity

--- src/test/java/edu/cofc/csis658/test/ArrayList2_ensureCapacity_Test.java
+++ src/test/java/edu/cofc/csis658/test/ArrayList2_ensureCapacity_Test.java
@@ -2,7 +2,7 @@ package edu.cofc.csis658.test;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.HashSet;
+import java.lang.reflect.Field;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,31 +10,39 @@ import org.junit.Test;
 public class ArrayList2_ensureCapacity_Test {
 	
 	int minCapacity;
+	int finalCapacity;
 	
 	@Before
 	public void setup() {
-
+		minCapacity = -1;
+		finalCapacity = 0;
 		
 	}
 
 	@Test
-	public void addAllNumMovedGreaterZero() {
-		System.out.println("test begin");
+	public void minCapacity10000() {
+		minCapacity = 10000;
+		
 		ArrayList2<String> ar2 = new ArrayList2();
-		
-		ar2.add("string");
-		ar2.add("string2");
-		ar2.add("string3");
-		
-		setA.add("String1");
-		setA.add("String2");
-		setA.add("String3");
-		
-		boolean mod = ar2.addAll(1, setA);
 	
-		assertTrue(mod);
+		try {
+			ar2.ensureCapacity(minCapacity);
+			finalCapacity = getCapacity(ar2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue(finalCapacity == minCapacity);
 	
-		System.out.println("test end");
 	}
+	
+	/*
+	 * This method uses reflection to gain access to 'elementData' Object in
+	 * of ArrayList2 object.
+	 */
+	static int getCapacity(ArrayList2<?> l) throws Exception {
+        Field dataField = ArrayList2.class.getDeclaredField("elementData");
+        dataField.setAccessible(true);
+        return ((Object[]) dataField.get(l)).length;
+    }
 
 }
