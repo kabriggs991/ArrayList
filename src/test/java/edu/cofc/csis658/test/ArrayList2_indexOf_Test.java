ca5718701f0d8d90aaf839c2c082b42dbccfda9e
husnasidd <siddiquih@g.cofc.edu>
2016-04-14 19:18:38 -0400

    added test for indexOf

--- src/test/java/edu/cofc/csis658/test/ArrayList2_indexOf_Test.java
+++ src/test/java/edu/cofc/csis658/test/ArrayList2_indexOf_Test.java
@@ -29,9 +29,7 @@ public class ArrayList2_indexOf_Test {
 			testArray.add(6);
 		
 			int returnValue = testArray.indexOf(4);
-		
 			assertEquals(1,returnValue);
-		
 			System.out.println("test end");
 		}
 		
@@ -46,9 +44,7 @@ public class ArrayList2_indexOf_Test {
 			testArray.add(6);
 		
 			int returnValue = testArray.indexOf(3);
-		
-			assertEquals(-1,returnValue);
-		
+			assertEquals(-1,returnValue);		
 			System.out.println("test end");
 		}
 		
@@ -62,13 +58,25 @@ public class ArrayList2_indexOf_Test {
 			testArray.add(null);
 			testArray.add(6);	
 		
-			int returnValue = testArray.indexOf(null);
-		
+			int returnValue = testArray.indexOf(null);	
 			assertEquals(2,returnValue);
-		
 			System.out.println("test end");
 		}
 		
+		@Test
+		public void indexOfNullNotNullInList() {
+			System.out.println("test begin");
+			ArrayList2<Integer> testArray = new ArrayList2();
+			
+			testArray.add(1);
+			testArray.add(4);
+			testArray.add(3);
+			testArray.add(6);	
+		
+			int returnValue = testArray.indexOf(null);	
+			assertEquals(-1,returnValue);
+			System.out.println("test end");
+		}
 
 	
 }
